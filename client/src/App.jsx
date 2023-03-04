2f21507c2cf3b2e50335650a5a7b7fb54948cbcc
Yurii Rybachok <drummer2009@ukr.net>
2023-03-04 09:44:48 +0200

    project file structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import { logo } from './assets';
+import { Home, CreatePost } from './pages';
 
 const App = () => {
   return (
-    <h1 className='text-3xl font-bold underline'>
-      Hello world!
-    </h1>
+    <BrowserRouter>
+    </BrowserRouter>
   )
 }
 
