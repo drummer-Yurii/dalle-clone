33f8c796ed190a304f24da8d54eeb98d2b3b3bff
Yurii Rybachok <drummer2009@ukr.net>
2023-03-05 11:52:14 +0200

    load post to community

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -16,6 +16,30 @@ const Home = () => {
     const [loading, setLoading] = useState(false);
     const [allPosts, setAllPosts] = useState(null);
     const [searchText, setSearchText] = useState('');
+
+    useEffect(() => {
+        const fetchPosts = async () => {
+            setLoading(true);
+            try {
+                const response = await fetch('http://localhost:8080/api/v1/post', {
+                  method: 'GET',
+                  headers: {
+                    'Content-Type': 'application/json',
+                  },
+                });
+                if(response.ok) {
+                    const result = await response.json();
+                    setAllPosts(result.data.reverse());
+                }
+            } catch (error) {
+                alert(error)
+            } finally {
+                setLoading(false);
+            }
+        }
+        fetchPosts();
+    }, []);
+
   return (
     <section className='max-w-7xl mx-auto'>
         <div>
@@ -49,7 +73,7 @@ const Home = () => {
                             />
                         ) : (
                             <RenderCards 
-                                data={[]}
+                                data={allPosts}
                                 title='No posts found'
                             />
                         )}
